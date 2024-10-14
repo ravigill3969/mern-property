b5864145b80b6e367892c8a63acc9312a2836321
Ravisher Singh <110797269+ravigill3969@users.noreply.github.com>
2024-10-14 12:49:17 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://mern-property-backend.onrender.com",
+    origin: "https://mern-property-frontend.onrender.com",
     credentials: true,
   })
 );
